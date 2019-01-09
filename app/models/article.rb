2ae79fb340708a7e7735925d3ea35f25ee407f94
Alvaro Sainz-Pardo <alvarosainzpardo@gmail.com>
2019-01-09 18:59:06 +0100

    refactoring-legacy-code-new-feature

--- app/models/article.rb
+++ app/models/article.rb
@@ -104,10 +104,10 @@ class Article < Content
     end
 
     def search_with_pagination(search_hash, paginate_hash)
-      
+
       state = (search_hash[:state] and ["no_draft", "drafts", "published", "withdrawn", "pending"].include? search_hash[:state]) ? search_hash[:state] : 'no_draft'
-      
-      
+
+
       list_function  = ["Article.#{state}"] + function_search_no_draft(search_hash)
 
       if search_hash[:category] and search_hash[:category].to_i > 0
@@ -414,6 +414,17 @@ class Article < Content
 
   def access_by?(user)
     user.admin? || user_id == user.id
+  end
+
+  def merge_with(article_to_merge)
+    self.body += article_to_merge.body
+    self.extended += article_to_merge.extended
+    self.save!
+    article_to_merge.comments.each do |comment|
+      self.comments << comment
+    end
+    self.save!
+    article_to_merge.save!
   end
 
   protected
